42a8ee770c16298cc7a60786ce097b8f14527f79
RotichKipkoech <kipkoechrottich@gmail.com>
2024-11-26 15:05:32 +0300

    Created a PDF

---
+++ app.py
@@ -0,0 +1,11 @@
+from reportlab.pdfgen import canvas
+
+pdf_file = canvas.Canvas("clcodingpdff.pdf")
+pdf_file.drawString(72, 720, "Hello, World!")
+pdf_file.drawString(72, 700, "Fre PDF Document ")
+pdf_file.drawString(72, 680, "Like | Share ")
+pdf_file.drawString(72, 660, "Subscribe ")
+pdf_file.drawString(72, 640, "Kennedy Rotich ")
+pdf_file.drawString(72, 620, "Thank you ")
+
+pdf_file.save()
